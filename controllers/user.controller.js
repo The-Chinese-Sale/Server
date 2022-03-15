efac9b72a9b9908d4df696316854b55eb07831bb
shulamitShulman <s0548543873@gmail.com>
2022-03-15 15:53:44 +0200

    feat()

---
+++ controllers/user.controller.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const router = express.Router()
+
+
+
+// router.get('/users/:id', async function (req, res) {
+//     res.send("Welcome")
+// })
+
+// router.post('/users', async function (req, res) {
+
+// })
+
+// module.exports = router;
